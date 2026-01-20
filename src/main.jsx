614c1c427ed968a7ce29ab8d2633b3e7603b6cd2
Sjoerd Vellinga <svellinga@Sjoerds-MacBook-Air.local>
2026-01-20 01:33:32 +0100

    fix(map): add react-leaflet + leaflet deps and include leaflet css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Track from "./pages/Track.jsx";
+import "leaflet/dist/leaflet.css";
 
 function AppShell() {
   return (
